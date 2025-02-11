44a4828541f01c36077c3cbfe067222e0bcc317b
☎️ <113849533+hhozaV@users.noreply.github.com>
2025-02-11 14:04:23 +0100

    prettierExec

--- coucou.js
+++ coucou.js
@@ -1,15 +1,14 @@
-function coucou(){
-    console.log("Coucou, c'est moi, le changement !");
-  }
-  coucou();
-  
-  function coucouPerso(name){
-    console.log(`Coucou, ${name}!`);
-  }
-  coucouPerso('Martin Boudras');
-  
-  function coucouQuote(){
-    console.log("Coucou, je préfère les quotes simples !");
-  }
-  coucouQuote();
-  
+function coucou() {
+  console.log("Coucou, c'est moi, le changement !");
+}
+coucou();
+
+function coucouPerso(name) {
+  console.log(`Coucou, ${name}!`);
+}
+coucouPerso('Martin Boudras');
+
+function coucouQuote() {
+  console.log('Coucou, je préfère les quotes simples !');
+}
+coucouQuote();
